e039522ad861c0b8bc9733dbabcd802a82c5207d
ionattan <imendez@geeksolutions.mx>
2015-11-15 21:25:41 -0700

    conectar bd y menu

---
+++ php/config.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+$host = "localhost";
+$usuario = "geek";
+$password = "crack123";
+$database = "kekito";
+
+$conexion=new mysqli($host,$usuario,$password,$database);
+if(mysqli_connect_errno()){
+  echo "no se pudo conectar";
+}
+$usuarios=$conexion->query("select * from usuarios");
+$servicios=$conexion->query("select * from servicios");
+$dispositivos=$conexion->query("select * from dispositivos");
+$facturas=$conexion->query("select * from facturas");
---
+++ www/index.php
@@ -0,0 +1,34 @@
+<?php
+include("../php/config.php");
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>iniciar sesion</title>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12">
+        <?php
+          include("../views/menu.html");
+          $seccion=isset($_GET["seccion"])?$_GET["seccion"]:"";
+          if($seccion){
+            include("../views/$seccion.html");
+          }
+        ?>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
