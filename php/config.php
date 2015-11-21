f82ad58ceefcc51d7bbb43859de47f8aa14982e7
ionattan mendez <imendez@geeksolutions.mx>
2015-11-20 18:46:14 -0700

    actualizacion de configuracion

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 $host = "localhost";
 $usuario = "geek";
-$password = "crack123";
+$password = "z98xxx53";
 $database = "kekito";
 
 $conexion=new mysqli($host,$usuario,$password,$database);
